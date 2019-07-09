7863b818d99278ff923bf0ae3bb14c9a1974030f
yosukekinoshita <yosukekinoshita1012@gmail.com>
2019-07-10 00:08:23 +0900

    テスト用createファイル作成

--- resources/views/to_companies/create.blade.php
+++ resources/views/to_companies/create.blade.php
@@ -12,17 +12,16 @@
     </head>
     <body>
         <h3>create</h3>
-        <form action="#" method="POST">
-            <!-- {{ csrf_field() }} -->
-            <input type="text" name="company_name">
-            <input type="text" name="company_name">
-            <input type="text" name="company_name">
-            <input type="text" name="company_name">
-            <input type="text" name="company_name">
-            <input type="text" name="company_name">
-            <input type="text" name="company_name">
-            <input type="text" name="company_name">
-            <input type="text" name="company_name">
-
+        <form action="{{ url('/to_companies')}}" method="POST">
+            {{ csrf_field() }}
+            <p>会社名 <input type="text" name="company_name"></p>
+            <p>住所1 <input type="text" name="address_1"></p>
+            <p>住所2 <input type="text" name="address_2"></p>
+            <p>電話番号1 <input type="text" name="telephone_1"></p>
+            <p>電話番号2 <input type="text" name="telephone_2"></p>
+            <p>電話番号3 <input type="text" name="telephone_3"></p>
+            <p>カテゴリー <input type="text" name="categories"></p>
+            <p>問い合わせURL <input type="text" name="contact_url"></p>
+            <button type="submit">送信</button>
     </body>
 </html>
